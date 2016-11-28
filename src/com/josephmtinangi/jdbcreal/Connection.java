55f657c773d0528f64135831f40640a353b1a9d1
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-28 15:15:11 +0300

    Retrieve data from the database

--- src/com/josephmtinangi/jdbcreal/Connection.java
+++ src/com/josephmtinangi/jdbcreal/Connection.java
@@ -4,13 +4,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Connection {
 
 	public static void main(String args[]) {
-		java.sql.Connection conn;
+		Scanner input = new Scanner(System.in);
+		java.sql.Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
 
 		Properties props = new Properties();
 		try {
@@ -31,9 +37,34 @@ public class Connection {
 				conn = DriverManager.getConnection(dbUrl, username, password);
 
 				System.out.println("Connection Successful.");
-				
-				
-				
+
+				int length = 0;
+				String comp_op = "=";
+
+				System.out.println("(<, >, =)");
+				comp_op = input.nextLine();
+				System.out.print("Length:");
+				length = input.nextInt();
+
+				String sql;
+				if (comp_op.equals(">")) {
+					sql = "SELECT film.* FROM film WHERE film.length > ?";
+				} else if (comp_op.equals("<")) {
+					sql = "SELECT film.* FROM film WHERE film.length < ?";
+				} else {
+					sql = "SELECT film.* FROM film WHERE film.length = ?";
+				}
+
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, length);
+
+				rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					System.out.println(rs.getInt("film_id") + "\t" + rs.getString("title") + "\t\t"
+							+ rs.getString("description") + "\t\t" + rs.getInt("length"));
+				}
+
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -45,6 +76,6 @@ public class Connection {
 
 			e.printStackTrace();
 		}
-
+		input.close();
 	}
 }
